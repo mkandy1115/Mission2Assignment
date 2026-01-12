fb69935e282e9c5f5bf33283cfd0c833f15644c9
Matthew Andrus <kyo.andrus@gmail.com>
2026-01-12 14:49:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Mission2Assignment;
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.Write("Welcome to the dice throwing simulator!\nHow many dice rolls would you like to simulate?\nNumber?:");
+        string userInput = System.Console.ReadLine();
+        int numRolls = int.Parse(userInput);
+
+        RollDices Dices = new RollDices(numRolls);
+        decimal[] percentages = Dices.SimulateRolls();
+
+        System.Console.WriteLine(
+            "\nDICE ROLLING SIMULATION RESULTS\n" +
+            "Each \"*\" represents 1% of the total number of rolls." +
+            "\nTotal number of rolls = " + numRolls + ".\n"
+            );
+
+        for (int i = 2; i <= 12; i++) {
+            System.Console.Write(i.ToString() + ": ");
+            for (int j = 0; j < (int)Math.Round(percentages[i]); j++) {
+                System.Console.Write("*");
+            }
+            System.Console.Write("\n");
+        }
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+    }
+}
---
+++ RollDices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2Assignment
+{
+    internal class RollDices
+    {
+        int numRolls;
+        public RollDices()
+        {
+            Random rand = new Random();
+            numRolls = rand.Next();
+        }
+
+        public RollDices(int userNum)
+        {
+            numRolls = userNum;
+        }
+
+        public decimal[] SimulateRolls()
+        {
+            int[] rollCounts = new int[13]; // index 0 and 1 will be unused
+            Random rand = new Random();
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = rand.Next(1, 7);
+                int die2 = rand.Next(1, 7);
+                int sum = die1 + die2;
+                rollCounts[sum]++;
+            }
+            decimal[] percentages = new decimal[13];
+            for (int i = 2; i <= 12; i++)
+            {
+                percentages[i] = (decimal)rollCounts[i] / numRolls * 100;
+            }
+            return percentages;
+        }
+
+    }
+}
